506de74de9ca29ef77a7e2d273aa93b8bd7f8c27
federicosimone <federico.simone15@gmail.com>
2025-11-08 11:18:38 +0100

    terzo prove

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -13,8 +13,7 @@ function iniziali(array) {
     for (i=0 ; i<array.length; i++) {
         
         newArray.push(array[i].charAt(0)) ;   
-        /*let newArray= [] ;
-        let firstLetter = newArray.push(scomposto[0]) ; */   
+         
     }
     //output 
     return newArray
--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,32 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+let lettera = prompt("Scegli la lettera per filtrare") ;
 
 // Dichiara la funzione qui.
 
+//input
+function letterFilter(array, string) {
+    let newArray= [] ;
+    
 
+    //elaborazione dati
+
+    for (i=0 ; i< array.length; i++) {
+        let firstLetter = array[i].charAt(0) ;
+        
+        if ( firstLetter == string ) {
+            
+            newArray.push(array[i]) ; 
+        }
+    } 
+
+    //output
+
+    return newArray
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(letterFilter(names, lettera)) ;
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
