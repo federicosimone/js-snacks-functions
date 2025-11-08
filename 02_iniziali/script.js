3a5f7a20397eb1f4f2a7a6b5f47f068b0802b94d
federicosimone <federico.simone15@gmail.com>
2025-11-08 10:39:27 +0100

    secondo esercizio

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function iniziali(array) {
+    //input
+    let newArray = []
+
+    //elaborazione dati 
+    for (i=0 ; i<array.length; i++) {
+        
+        newArray.push(array[i].charAt(0)) ;   
+        /*let newArray= [] ;
+        let firstLetter = newArray.push(scomposto[0]) ; */   
+    }
+    //output 
+    return newArray
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(iniziali(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
